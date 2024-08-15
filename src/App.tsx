322beb10b4219e98cf17d4ec8fea6a62439911c6
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-08-16 04:25:00 +0430

    useState hook added to App.tsx file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import CourseGoal from "./components/CourseGoal.tsx";
 import goalsImg from "./assets/goals.jpg";
 import Header from "./components/Header.tsx";
+import { useState } from "react";
+
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
 
 export default function App() {
+  const [goals, setGoals] = useState<Array<CourseGoal>>([]);
+
+  function handleAddGoal() {
+    // setGoals();
+  }
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your Course Goal</h1>
       </Header>
+      <button onClick={handleAddGoal}>Add Goal</button>
       <CourseGoal title="React + TypeScript">
         <p>Starting from ground up</p>
       </CourseGoal>
